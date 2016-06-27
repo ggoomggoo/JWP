d107016529c56976a88137773972fc7961ee3317
t1t1 <cha@hanmail2.net>
2016-06-27 20:38:50 +0900

    junit Before

--- calculator/test/calculator/CalculatorTest.java
+++ calculator/test/calculator/CalculatorTest.java
@@ -3,13 +3,21 @@ package calculator;
 // import static *
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class CalculatorTest {
 	
+//	Calculator calculator = new Calculator();
+	Calculator calculator;
+	
+	@Before
+	public void setup() {
+		calculator = new Calculator();
+	}
+	
 	@Test
 	public void testAdd() {
-		Calculator calculator = new Calculator();
 		int result = calculator.add(3, 4);
 //		System.out.println(result);
 		assertEquals(7, result);
@@ -17,7 +25,6 @@ public class CalculatorTest {
 	
 	@Test
 	public void testMinus() {
-		Calculator calculator = new Calculator();
 		assertEquals(1, calculator.minus(4, 3));
 	}
 
